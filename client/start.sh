20d859e5bb749516714ce3be5f24ccd0a0efafce
Santosh Shrestha <santosh@rumsan.com>
2025-12-07 21:24:28 +0545

    add start script

---
+++ client/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Base URL for the repository
+BASE_URL="https://raw.githubusercontent.com/santosh-rumsan/rumsan-ssh-setup/refs/heads/main"
+
+# Create rs-ssh-app folder
+mkdir -p rs-ssh-app
+cd rs-ssh-app
+
+# Download rs_client.sh
+curl -O ${BASE_URL}/client/rs_client.sh
+
+# Download oauth_login.py
+curl -O ${BASE_URL}/client/oauth_login.py
+
+# Make rs_client.sh executable
+chmod +x rs_client.sh
+
+# Run rs_client.sh
+./rs_client.sh
